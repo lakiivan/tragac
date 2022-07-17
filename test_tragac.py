c490fd579ce9a58e552ad931bcd06b0a29f66edf
lakiivan <ivanzlazic@gmial.com>
2022-07-17 16:56:42 +0200

    dodat unit test za scriptu tragac u skripti tragac dodata funkcija za dodavanje leading nula part numbera ako se zbog formatiranja izgube u csv fajlu

--- test_tragac.py
+++ test_tragac.py
@@ -1,10 +1,40 @@
 import unittest
 
+import tragac
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
+class TestTragacFunctions(unittest.TestCase):
+
+    def test_dodaj_nule_sipred_pn(self):
+        pn = "1234"
+        expected = "01234"
+        actual = tragac.dodaj_nule_ispred_pn(pn)
+        self.assertEqual(expected, actual, "Test ddoaj nule ispred pn Failed for " + pn)  # add assertion here
+
+        pn = "234"
+        expected = "00234"
+        actual = tragac.dodaj_nule_ispred_pn(pn)
+        self.assertEqual(expected, actual, "Test ddoaj nule ispred pn Failed for " + pn)  # add assertion here
+
+        expected = "00034"
+        pn = "34"
+        actual = tragac.dodaj_nule_ispred_pn(pn)
+        self.assertEqual(expected, actual, "Test ddoaj nule ispred pn Failed for " + pn)  # add assertion here
+
+        pn = "4"
+        expected = "00004"
+        actual = tragac.dodaj_nule_ispred_pn(pn)
+        self.assertEqual(expected, actual, "Test ddoaj nule ispred pn Failed for " + pn)  # add assertion here
+
+        pn = ""
+        expected = "00000"
+        actual = tragac.dodaj_nule_ispred_pn(pn)
+        self.assertEqual(expected, actual, "Test ddoaj nule ispred pn Failed for " + pn)  # add assertion here
+
+        pn = "123456"
+        expected = pn
+        actual = tragac.dodaj_nule_ispred_pn(pn)
+        self.assertEqual(expected, actual, "Test ddoaj nule ispred pn Failed for " + pn)  # add assertion here
 
 if __name__ == '__main__':
     unittest.main()
